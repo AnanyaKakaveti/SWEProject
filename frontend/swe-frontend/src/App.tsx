6297d2f0844adb83fbda761809a80ffb450114cf
Amandasmith930 <92866224+Amandasmith930@users.noreply.github.com>
2023-02-08 00:52:32 -0500

    updated Login and Register pages with accepting user auth

--- frontend/swe-frontend/src/App.tsx
+++ frontend/swe-frontend/src/App.tsx
@@ -1,21 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Login from "./components/Login";
 import { Connect, SendMessage } from "./api";
+import Nav from "./components/nav";
+import Home from "./components/Home";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Register from "./components/Register";
 
-const App: React.FC = () => {
+
+function App(){
+
 useEffect(() => {
 Connect();
 }, []);
 
-const handleClick = () => {
+const handleClick = () => { 
 console.log("hello");
 SendMessage("hello");
 };
 
-return (
-<div className="App">
-<button onClick={handleClick}>Hit</button>
-</div>
+return(
+     <div className="App">    
+        <BrowserRouter>
+        <Nav/>
+        <main className="form-signin w-100 m-auto">
+         <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/Register" element={<Register/>} />
+            </Routes>  
+    </main>
+    </BrowserRouter>
+    <button onClick={handleClick}>Hit</button>
+        </div>
 );
 };
 
---
+++ frontend/swe-frontend/src/api/index.tsx
@@ -0,0 +1,28 @@
+const socket = new WebSocket("ws://localhost:8080/ws");
+
+const Connect = () => {
+  console.log("Attempting Connection...");
+
+  socket.onopen = () => {
+    console.log("Successfully Connected");
+  };    
+
+  socket.onmessage = (msg: MessageEvent) => {
+    console.log(msg.data);
+  };
+
+  socket.onclose = (event: CloseEvent) => {
+    console.log(`Socket Closed Connection: ${event}`);
+  };
+
+  socket.onerror = (error: Event) => {
+    console.log(`Socket Error: ${error}`);
+  };
+};
+
+const SendMessage = (msg: string) => {
+  console.log(`Sending msg: ${msg}`);
+  socket.send(msg);
+};
+
+export { Connect, SendMessage }
